b22f45305c26d83dd6ee3c94f4a93afcc80f0a1a
unknown <shirley@ipayafrica.com>
2023-02-11 09:47:14 +0300

    adding encoding types

---
+++ components/EncodingTypes/index.jsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import { Stack, Box } from "@mui/material";
+
+const EncodingTypes = () => {
+  const [alignment, setAlignment] = React.useState("url");
+
+  const handleAlignment = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
+  const values = [
+    { type: "URL", value: "url" },
+    { type: "Email", value: "email" },
+    { type: "SMS", value: "sms" },
+    { type: "Wi-Fi", value: "wifi" },
+  ];
+
+  return (
+    <Stack alignItems="center">
+      <ToggleButtonGroup
+        orientation="vertical"
+        value={alignment}
+        exclusive
+        onChange={handleAlignment}
+        aria-label="text alignment"
+      >
+        {Object.entries(values).map(([index, value]) => (
+          <ToggleButton
+            value={value.value}
+            aria-label={value.value}
+            key={value.value}
+          >
+            <Box>{value.type}</Box>
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
+    </Stack>
+  );
+};
+
+export default EncodingTypes;
